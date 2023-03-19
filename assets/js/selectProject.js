bc1b721439fc4af264b419383e8f2e985986c1c9
andreyrickdev <contato-andrey@outlook.com>
2023-03-19 17:33:15 -0300

    removed old codes

--- assets/js/selectProject.js
+++ assets/js/selectProject.js
@@ -16,54 +16,6 @@ function openProject () {
         })
     }
 
-    /*projetos[0].addEventListener("click", function(){
-
-        dadBox[0].classList.toggle('boxForP-auto');
-        boxForP[0].classList.toggle('boxForP-display');
-        fechar[0].classList.toggle('x-hide');
-    });
-
-    projetos[1].addEventListener("click", function(){
-
-        dadBox[1].classList.toggle('boxForP-auto');
-        boxForP[1].classList.toggle('boxForP-display');
-        fechar[1].classList.toggle('x-hide');
-    });
-
-    projetos[2].addEventListener("click", function(){
-
-        dadBox[2].classList.toggle('boxForP-auto');
-        boxForP[2].classList.toggle('boxForP-display');
-        fechar[2].classList.toggle('x-hide');
-    });
-
-    projetos[3].addEventListener("click", function(){
-
-        dadBox[3].classList.toggle('boxForP-auto');
-        boxForP[3].classList.toggle('boxForP-display');
-        fechar[3].classList.toggle('x-hide');
-    });
-
-    projetos[4].addEventListener("click", function(){
-
-        dadBox[4].classList.toggle('boxForP-auto');
-        boxForP[4].classList.toggle('boxForP-display');
-        fechar[4].classList.toggle('x-hide');
-    });
-
-    projetos[5].addEventListener("click", function(){
-
-        dadBox[5].classList.toggle('boxForP-auto');
-        boxForP[5].classList.toggle('boxForP-display');
-        fechar[5].classList.toggle('x-hide');
-    });
-
-    projetos[6].addEventListener("click", function(){
-
-        dadBox[6].classList.toggle('boxForP-auto');
-        boxForP[6].classList.toggle('boxForP-display');
-        fechar[6].classList.toggle('x-hide');
-    });*/
 }
 
 openProject();
